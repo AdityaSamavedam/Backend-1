b03b52a8796b2f5501adebf2982b3be5a70705a7
sumedhaKl <sumedhakamaraju@gmail.com>
2023-10-17 00:15:41 -0700

    first commit

--- projects/script.js
+++ projects/script.js
@@ -1,18 +1,13 @@
-// Define an array of Shakespearean quotes or text.
-const shakespeareanTexts = [
-    "To be or not to be, that is the question.",
-    "All the world's a stage, and all the men and women merely players.",
-    "Love all, trust a few, do wrong to none.",
-    // Add more Shakespearean texts here
-];
+!DOCTYPE <html>
+<html>
+<head>
+    <title>Shakespearean Text Printer</title>
+</head>
+<body>
+    <h1>Shakespearean Text</h1>
+    <p id="shakespeareText">Click the button to display Shakespearean text here.</p>
+    <button id="printButton">Print Shakespeare</button>
 
-// Function to display a random Shakespearean text when the button is clicked.
-function printShakespeare() {
-    const shakespeareTextElement = document.getElementById("shakespeareText");
-    const randomIndex = Math.floor(Math.random() * shakespeareanTexts.length);
-    const randomText = shakespeareanTexts[randomIndex];
-    shakespeareTextElement.textContent = randomText;
-}
-
-// Attach the click event listener to the button.
-document.getElementById("printButton").addEventListener("click", printShakespeare);
+    <script src="script.js"></script>
+</body>
+</html>
